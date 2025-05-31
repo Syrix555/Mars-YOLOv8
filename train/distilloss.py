776c1885bb496d558e8a97629f9ae24c93964d7a
mmm <1626280993@qq.com>
2025-05-31 21:43:29 +0800

    完成知识蒸馏

--- train/distilloss.py
+++ train/distilloss.py
@@ -1,15 +1,24 @@
 import torch
 from overrides import override # this could be removed since Python 3.12
-
+from .loss import DetectionLoss, CWDLoss,  ResponseLoss
 
 class DistillationDetectionLoss(object):
     def __init__(self, mcfg, model):
         self.mcfg = mcfg
         self.histMode = False
-        # self.detectionLoss = DetectionLoss(mcfg, model)
+        self.detectionLoss = DetectionLoss(mcfg, model)
         # self.cwdLoss = CWDLoss(self.mcfg.device)
-        # self.respLoss = ResponseLoss(self.mcfg.device, self.mcfg.nc, self.mcfg.teacherClassIndexes)
-        raise NotImplementedError("DistillationDetectionLoss::__init__")
+        cwd_temperature = getattr(mcfg, 'cwd_temperature', 1.0)  # 从mcfg获取温度，若无则默认为1.0
+        cwd_spatial_kl_reduction = getattr(mcfg, 'cwd_spatial_kl_reduction', 'batchmean') # 从mcfg获取，若无则用默认值
+
+        self.cwdLoss = CWDLoss(
+            temperature=cwd_temperature,
+            spatial_kl_reduction=cwd_spatial_kl_reduction
+        )
+        self.respLoss = ResponseLoss(self.mcfg.device, self.mcfg.nc, self.mcfg.teacherClassIndexes, self.mcfg.regMax)
+
+        # raise NotImplementedError("DistillationDetectionLoss::__init__")
+
 
     @override
     def __call__(self, rawPreds, batch):
